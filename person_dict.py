b79066722b3733b09a0905a438a850728cf7ff1a
mcdeedz <110458001+mcdeedz@users.noreply.github.com>
2023-01-17 15:51:45 +0100

    Add files via upload

---
+++ person_dict.py
@@ -0,0 +1,4 @@
+db = [{'name': 'Christian', 'longitude': 16.38347702493873, 'latitude': 48.18481583765468, 'city': 'Wien',
+       'email': 'christian.magdits@gmail.com'}]
+      #,{'name': 'Schnutti', 'longitude': 16.38347702493873, 'latitude': 48.18481583765468, 'city': 'Wien', 'email': 'julia.maschik@gmail.com'}]
+      #   {'name': 'Silvia', 'longitude': 16.246380670585104, 'latitude': 47.24784903959448, 'city': 'Rotenturm a.d. Pinka','email': 'silviamagdits@gmail.com'}]
